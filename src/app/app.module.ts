8dedae509f3f89224f8f7195f00a735a8b476fd3
Harena Andriamahatony <harena.andriamahatony@gmail.com>
2023-08-19 08:57:52 +0300

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    AcceuilComponent
+    AcceuilComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
