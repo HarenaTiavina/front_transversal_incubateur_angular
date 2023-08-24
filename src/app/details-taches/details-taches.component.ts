690c3cb2eea0b34bbfe20cc0a97d8764a97d8883
Harena Andriamahatony <harena.andriamahatony@gmail.com>
2023-08-24 16:03:26 +0300

    add details tache

--- src/app/details-taches/details-taches.component.ts
+++ src/app/details-taches/details-taches.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-details-taches',
   templateUrl: './details-taches.component.html',
   styleUrls: ['./details-taches.component.css']
 })
-export class DetailsTachesComponent {
+export class DetailsTachesComponent implements AfterViewInit {
+  @ViewChild('dataTableDepensesTache', { static: false }) table!: ElementRef;
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    $(this.table.nativeElement).DataTable();
+  }
 }
